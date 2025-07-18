cca7ef1ea9e1bf6c3ba36ff3b8e175306a940432
shaman2016scratch <g.ilkh@yandex.ru>
2025-07-18 22:54:19 +0300

    Create index.py

---
+++ files/index.py
@@ -0,0 +1,1 @@
+print("https://raw.githubusercontent.com/shaman2016scratch/shaman2016.github.io/refs/heads/main/files/%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B8/%D0%90%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D0%B0%201.2.jpg")
